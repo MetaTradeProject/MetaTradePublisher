401f2be38fb4672376a483735e3451813ce6106c
Freesia810 <z1402121306@hotmail.com>
2023-05-24 00:20:53 +0800

    modified:   src/main/java/com/freesia/metatradepublisher/jni/JniSigner.java 	modified:   src/main/resources/item_list.json 	modified:   src/main/resources/store_list.json

--- src/main/java/com/freesia/metatradepublisher/jni/JniSigner.java
+++ src/main/java/com/freesia/metatradepublisher/jni/JniSigner.java
@@ -5,12 +5,12 @@ import com.freesia.metatradepublisher.MetaTradePublisherApplication;
 public class JniSigner{
     static {
         if (MetaTradePublisherApplication.class.getResource("").getProtocol().equals("jar")) {
-            String path = System.getProperty("user.dir") + "/lib/libsigner.so";
+            String path = System.getProperty("user.dir") + "/lib/libsigner_pub.so";
             System.load(path);
         }
         else{
             String path = JniSigner.class.getClassLoader().getResource("//").getPath();
-            System.load(path + "libsigner.so");
+            System.load(path + "libsigner_pub.so");
         }
         
     }
